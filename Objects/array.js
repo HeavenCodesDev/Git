8f23e69f6ab61caa68547a18ea89d76eeb0e329a
HeavenCodesDev <heevenCaalaa11@gmail.com>
2026-09-15 13:24:20 -0500

    looping through an array added on array file

--- Objects/array.js
+++ Objects/array.js
@@ -11,3 +11,7 @@ console.log(array1[1]);
 
 const array2 = [1,2,3,4];
 console.log(array2);
+for(let i= 0;i<array2.length;i++){
+  console.log(array2[i]);
+}
+console.log('looping through an array');
